e79a30f22dbc1f07288d10d0edd80867a2e0f2fb
bellyxs <bellarodgs@gmail.com>
2023-11-08 23:02:08 -0300

    aaaaaaaaaaaaaaaaaaaaaaa

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Começo do site do banco digital talvez?</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Começo do site do banco digital talvez?</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="#">Página Inicial</a></li>
+            <li><a href="#">Sobre Nós</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+    </nav>
 
-session_start();
-
-include './config.php';
-include './autoload.php';
-include './rotas.php';
+    <section>
+        <article>
+            <h2>lesgo banco digital</h2>
+        </article>
+    </section>
+</body>
+</html>
